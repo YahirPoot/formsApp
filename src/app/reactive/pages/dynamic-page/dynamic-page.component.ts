26eb2e6f9d7d912cc1cc6cee895543090b7dd9c2
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-11-11 17:49:26 -0600

    inicio de formularios dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
+
+  public myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Hollow Knight', Validators.required],
+      ['Celeste', Validators.required],
+      ['Dead Cells', Validators.required]
+    ]),
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
